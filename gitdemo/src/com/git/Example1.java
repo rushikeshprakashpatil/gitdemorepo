ac75443a346a53d28ab0e4d7eaf00f759b5adcbb
sachin5417 <101930885+sachin5417@users.noreply.github.com>
2022-12-24 16:43:02 +0530

    first version

---
+++ gitdemo/src/com/git/Example1.java
@@ -0,0 +1,16 @@
+package com.git;
+
+import java.util.ArrayList;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		ArrayList al =new ArrayList();
+		al.add(10);
+		al.add(20);
+		System.out.println(al);
+		
+
+	}
+
+}
